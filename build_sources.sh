42be9dd2a2de2a9b072058ae27253cd7a1572da3
Markus Zoeller <mzoeller@de.ibm.com>
2017-09-11 19:37:56 -0600

    The build script uses a virtualenv for package isolation
    Nothing more to say here. I did version pinning in the requirements
    file, it doesn't make sense to do a global install of pinned
    packages. Within an virtualenv on the other hand, it's ok.

--- build_sources.sh
+++ build_sources.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+virtualenv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+
 rm -rf ./.doctrees/
 rm -rf ./_website/
 ablog build
 
-echo "The newly built blog content is ready:"
-ls -la _website
+deactivate
