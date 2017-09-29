7bc997a52f60f3421119a5afcbd8099ba0ec7f50
Markus Zoeller <mzoeller@de.ibm.com>
2017-09-29 13:14:44 +0200

    The APT packages need to be installed in an earlier phase

--- install-apt-packages.sh
+++ install-apt-packages.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+apt-get -qq update
 apt-get install -y python-enchant  # for pypi package sphinxcontrib-spelling
 apt-get install -y graphviz        # for sphinx directive graphviz
