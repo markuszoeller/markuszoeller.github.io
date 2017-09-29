67825e2b3a0158e81c961b90c5ba7c99b079cba0
Markus Zoeller <mzoeller@de.ibm.com>
2017-09-29 11:39:06 +0200

    Change the html title displayed in the browser

--- conf.py
+++ conf.py
@@ -271,7 +271,7 @@ html_theme_path = [alabaster.get_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = "Markus Zoeller Blog"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
