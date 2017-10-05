a2045a3d08b8664e795a1c3aca7cfdc67409ad8d
markus.zoeller <zoeller.markus@web.de>
2017-10-05 19:34:09 +0200

    Preparation to allow private fixes to ABlog
    There is a bug in ABlog which might not be fixed fast enough.
    This bug (bug report TBD) allows that posts, pre-dated to tomorrow
    are published today. Details in the upcoming patch.
    This change here should give me the insides to apply the private patch.

--- build_sources.sh
+++ build_sources.sh
@@ -6,6 +6,14 @@ pip install -r requirements.txt
 
 rm -rf ./.doctrees/
 rm -rf ./_website/
+
+# workaround --->
+echo "=== MZO: apply private fixes"
+echo "Trying to find the file to patch..."
+ls -l .venv/local/lib/python2.7/site-packages/ablog/blog.py
+find / -name blog.py -type f 2>/dev/null
+# <--- workaround
+
 ablog build
 
 deactivate
