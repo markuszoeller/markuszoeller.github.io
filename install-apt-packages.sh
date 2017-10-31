3d9bc1ef1835759f2b73197203b4e5566f7ab991
markus.zoeller <zoeller.markus@web.de>
2017-10-31 15:15:07 +0100

    21: Use Makefile for the daily business
    This drops all the little bash scripts and
    updates the README to make sure the make commands
    get used now.
    
    Closes #21

--- install-apt-packages.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-apt-get -qq update
-apt-get install -y python-enchant  # for pypi package sphinxcontrib-spelling
-apt-get install -y graphviz        # for sphinx directive graphviz
