586db7a89396a4be2b58e78f9b2eaa4770c53b1e
Markus Zoeller <mzoeller@de.ibm.com>
2017-11-21 15:07:30 +0100

    add asciinema recordings to ansible-playbook-roles post
    I still have to figure out the best size for the console
    and how I can automatize those things best.

--- conf.py
+++ conf.py
@@ -209,7 +209,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['README.rst', '.venv']
+exclude_patterns = ['README.rst', '.venv', '.asciinema']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
